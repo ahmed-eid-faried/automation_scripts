6b395ed2ce7bc6e5c326530859c3a13f9b526ed6
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-14 14:06:32 +0300

    Create bash clean_build_files_visial_studio.sh

---
+++ bash clean_build_files_visial_studio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# حذف الملفات المترجمة من مشروع C++ أو C#
+find . -type d -name "Debug" -exec rm -rf {} +;
+find . -type d -name "Release" -exec rm -rf {} +;
+find . -type d -name ".vs" -exec rm -rf {} +;
+find . -type d -name "x64" -exec rm -rf {} +;
+find . -type d -name "x86" -exec rm -rf {} +;
+find . -type d -name "bin" -exec rm -rf {} +;
+find . -type d -name "obj" -exec rm -rf {} +;
+
+echo "تم حذف الملفات المترجمة بنجاح!"
