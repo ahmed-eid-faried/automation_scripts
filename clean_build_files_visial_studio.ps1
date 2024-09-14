9956c88384fbc36515bcdd8faf49d14cbbbad07e
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-14 14:09:18 +0300

    Create clean_build_files_visial_studio.ps1

---
+++ clean_build_files_visial_studio.ps1
@@ -0,0 +1,12 @@
+//./clean_build_files.ps1
+
+# حذف الملفات المترجمة من مشروع C++ أو C#
+$folders = @("Debug", "Release", ".vs", "x64", "x86", "bin", "obj")
+
+foreach ($folder in $folders) {
+    Get-ChildItem -Recurse -Directory -Filter $folder | ForEach-Object { 
+        Remove-Item -Recurse -Force $_.FullName
+    }
+}
+
+Write-Host "تم حذف الملفات المترجمة بنجاح!"
